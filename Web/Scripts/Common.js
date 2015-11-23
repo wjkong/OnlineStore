a56ad78c13727867ca4a390da9ae015398da9513
wjkonger@gmail.com <wjkonger@gmail.com>
2015-11-22 22:26:24 -0500

    Complete Angular Routing

---
+++ Web/Scripts/Common.js
@@ -0,0 +1,51 @@
+﻿var app = angular.module('onlineStoreApp', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        // route for the home page
+        .when('/', {
+            templateUrl: 'Pages/home.html',
+            controller: 'mainController'
+        })
+         .when('/login', {
+             templateUrl: 'Pages/login.html',
+             controller: 'loginController'
+         })
+
+
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'Pages/about.html',
+            controller: 'aboutController'
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'Pages/contact.html',
+            controller: 'contactController'
+        });
+
+
+    //$locationProvider.html5Mode(true);
+});
+
+
+
+// create the controller and inject Angular's $scope
+app.controller('mainController', function ($scope) {
+
+    // create a message to display in our view
+    $scope.message = 'Everyone come and see how good I look!';
+});
+
+app.controller('aboutController', function ($scope) {
+    $scope.message = 'Look! I am an about page.';
+});
+
+app.controller('contactController', function ($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+});
+
+app.controller('loginController', function ($scope) {
+    $scope.message = 'Login .';
+});
