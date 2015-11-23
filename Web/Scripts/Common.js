2265985dca988c60c84860ce00fc0237a00e346a
wjkonger@gmail.com <wjkonger@gmail.com>
2015-11-22 23:10:54 -0500

    Create Login

--- Web/Scripts/Common.js
+++ Web/Scripts/Common.js
@@ -1,4 +1,91 @@
-﻿var app = angular.module('onlineStoreApp', ['ngRoute']);
+﻿var emptyStr = '';
+var zero = 0.00;
+var lineBreak = '<br />';
+var yes = "Y";
+var singleQuote = "'";
+var comma = ",";
+var space = " ";
+var root = emptyStr;
+var linkText = "<a target=_blank href='{0}'>{1}</a>";
+var mapLink = "<span data-latitude='{0}' data-longitude='{1}' class='map text-info' data-toggle='modal' data-target='#modalMap'><span class='glyphicon glyphicon-map-marker'></span>{2}</span>";
+var imgLink = "<img src='{0}' alt=icon />";
+
+(function (i, s, o, g, r, a, m) {
+    i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+        (i[r].q = i[r].q || []).push(arguments)
+    }, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+
+ga('create', 'UA-65517203-1', 'auto');
+ga('send', 'pageview');
+
+Date.prototype.yyyymmdd = function () {
+
+    var yyyy = this.getFullYear().toString();
+    var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
+    var dd = this.getDate().toString();
+    return yyyy + (mm[1] ? mm : "0" + mm[0]) + (dd[1] ? dd : "0" + dd[0]); // padding
+};
+
+Date.prototype.mmdd = function () {
+    var mm = (this.getMonth() + 1).toString(); // getMonth() is zero-based
+    var dd = this.getDate().toString();
+    return (mm[1] ? mm : "0" + mm[0]) + "/" + (dd[1] ? dd : "0" + dd[0]); // padding
+};
+
+String.prototype.format = function () {
+    var s = this;
+    var i = arguments.length;
+
+    while (i--) {
+        s = s.replace(new RegExp('\\{' + i + '\\}', 'gm'), arguments[i]);
+    }
+    return s;
+};
+
+
+String.prototype.formatNumber = function () {
+    var s = this;
+
+    return Number(s.replace(/[^0-9\.]+/g, ""));
+};
+
+
+$.fn.stars = function () {
+
+    var strStar = "<span style='width: {0}px;'></span>";
+
+    return $(this).each(function () {
+        // Get the value
+        var val = parseFloat($(this).html());
+        // Make sure that the value is in 0 - 5 range, multiply to get width
+        var size = Math.max(0, (Math.min(5, val))) * 16;
+        // Create stars holder
+        // Replace the numerical value with stars
+        $(this).html(strStar.format(size));
+    });
+}
+
+$.fn.ShowProgressIndicator = function () {
+    // To prevent double-click, create an invisible layer to cover the submit button. Also, display a progress indicator.
+    $(this).append("<span class='progressIndicator'><img src='../Images/loading.gif' /></span>");
+
+    var position = $(this).position();
+
+    var left = position.left - 2;
+    var top = position.top - 2;
+    var height = $(this).outerHeight() + 4;
+    var width = $(this).outerWidth() + 4;
+
+    $('.progressIndicator').css('left', left)
+                                      .css('top', top)
+                                      .css('width', width)
+                                      .css('height', height)
+                                      .show();
+}
+
+var app = angular.module('onlineStoreApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
@@ -49,3 +136,25 @@ app.controller('contactController', function ($scope) {
 app.controller('loginController', function ($scope) {
     $scope.message = 'Login .';
 });
+
+function GetParameterByName(name) {
+    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+    var regexS = "[\\?&]" + name + "=([^&#]*)";
+    var regex = new RegExp(regexS);
+    var results = regex.exec(window.location.search);
+    if (results == null)
+        return "";
+    else
+        return decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function setCookie(key, value) {
+    var expires = new Date();
+    expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+    document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+}
+
+function getCookie(key) {
+    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    return keyValue ? keyValue[2] : null;
+}
---
+++ Web/Scripts/Login.js
@@ -0,0 +1,81 @@
+﻿$(function () {
+    var userName = getCookie("userName");
+
+    if (userName != undefined && userName.length > 0) {
+        $('#txtUsername').val(userName);
+
+        $("#chkRememberMe").prop('checked', true);
+    }
+
+    $('#btnLogin').click(function () {
+        $(this).ShowProgressIndicator();
+
+        var username = $.trim($('#txtUsername').val());
+        var password = $.trim($('#txtPassword').val());
+        var response = grecaptcha.getResponse();
+
+        if (username.length > 0 && password.length > 0 && response.length > 0) {
+            var url = "Login.aspx/LoginUser";
+
+            var param = "{ 'username': '{0}', 'password': '{1}', 'response': '{2}' }";
+            param = param.format(username, password, response);
+            var str = [];
+
+            $.ajax({
+                type: 'POST',
+                url: url,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                data: param,
+                success: OnSuccess,
+                error: function (xhr, status, error) {
+                    PopupValidation(error);
+                },
+                complete: function (xhr, status) {
+                    grecaptcha.reset();
+                    $('.progressIndicator').fadeOut(100).remove();
+                }
+            });
+        }
+        else {
+            grecaptcha.reset();
+
+            var str = [];
+
+            if (username.length == 0)
+                str.push("<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>Username is requried<br/>");
+
+            if (password.length == 0)
+                str.push("<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>Password is requried<br/>");
+
+            if (response.length == 0)
+                str.push("<span class='glyphicon glyphicon-exclamation-sign' aria-hidden='true'></span>ReCaptcha is requried<br/>");
+
+            $('.progressIndicator').fadeOut(100).remove();
+
+            PopupValidation(str.join(emptyStr));
+        }
+    });
+
+
+});
+
+function OnSuccess(data, status) {
+    if (data.d == "OK") {
+        if ($("#chkRememberMe").prop('checked')) {
+            var username = $.trim($('#txtUsername').val());
+
+            setCookie("userName", username);
+        }
+
+        window.location.href = "Member/MyPhoto.aspx";
+    }
+    else {
+        PopupValidation("Invalid username and password");
+    }
+}
+
+function PopupValidation(content) {
+    $('#modalValidation .modal-body').html("<div class='alert alert-danger'>" + content + "</div>");
+    $('#modalValidation').modal('show');
+}
