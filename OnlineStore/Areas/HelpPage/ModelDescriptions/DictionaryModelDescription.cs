6d6a59d47365e64ef2e14a2d7490c054a087c2b9
wjkonger@gmail.com <wjkonger@gmail.com>
2015-11-28 18:25:12 -0500

    dfsf

--- OnlineStore/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace OnlineStore.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- OnlineStore/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace OnlineStore.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
